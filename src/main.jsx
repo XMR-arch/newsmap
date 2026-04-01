63efbe5b967ca4112da25232face8faaff5ffe3e
XMR-arch <ricardomonetti@aol.com>
2026-04-01 13:24:41 -0300

    feat: implementa slider de timeline para navegación día a día

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import App from './components/App.jsx'
+import App from './src/App.jsx'
 import './components/index.css' // Ruta corregida según tu estructura de archivos
 
 // Configuración del cliente de caché para las noticias
